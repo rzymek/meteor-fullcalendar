9c519b589f6356f6a5b6d5ebf29d20d020f9883a
rzymek <rzymek@gmail.com>
2014-12-26 13:08:49 +0100

    2.2.3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'rzymek:fullcalendar',
     summary: "Full-sized drag & drop event calendar (jQuery plugin)",
-    version: "2.2.0_2",
+    version: "2.2.3",
     git: "https://github.com/rzymek/meteor-fullcalendar.git"
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
     api.versionsFrom('METEOR@0.9.2.2');
     api.use([
         'mrt:jquery-ui@1.9.2',
-        'rzymek:moment@2.8.3_10',
+        'momentjs:moment@2.8.4',
         'templating'
     ], 'client');
     api.addFiles([
