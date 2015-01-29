2a90be09d28e784fb628b5f73c663ae906d0b08e
rzymek <rzymek@gmail.com>
2015-01-29 20:10:01 +0100

    example updated

--- example/client/example.js
+++ example/client/example.js
@@ -1,10 +1,32 @@
 Template.body.helpers({
-    options: function() {
+    options: function () {
         return {
             id: 'myid2',
-            class: 'myClass2',
+            class: 'myCalendars',
             lang: 'pl',
-            defaultView: 'basicWeek'
+            height: 300,
+            defaultView: 'basicWeek',
+            events: function (start, end, tz, callback) {
+                callback([{
+                        title: new Date().toGMTString(),
+                        start: new Date()
+                    }]);
+            }
         };
+    },
+    cal1Events: function () {
+        return function (start, end, tz, callback) {
+            callback([{
+                    title: new Date().toISOString(),
+                    start: new Date(),
+                    allDay: true
+                }]);
+        };
+    }
+});
+Template.body.events({
+    'click .refresh': function (e, template) {
+        //template.$('#myid2').fullCalendar('refetchEvents');
+        template.$('.myCalendars').fullCalendar('refetchEvents');
     }
 });
