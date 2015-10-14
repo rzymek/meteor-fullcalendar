c545beacfea35cbb2ea0392a66414ab09c5a9cb5
rzymek <rzymek@gmail.com>
2015-10-14 21:24:17 +0200

    example: include eventClick example (#15)

--- example/client/example.js
+++ example/client/example.js
@@ -1,25 +1,30 @@
-Template.body.helpers({
-    events: function () {
-        var fc = $('.fc');
-        return function (start, end, tz, callback) {
-            //subscribe only to specified date range
-            Meteor.subscribe('events', start.toDate(), end.toDate(), function () {
-                //trigger event rendering when collection is downloaded
-                fc.fullCalendar('refetchEvents');
-            });
+    Template.body.helpers({
+        events: function () {
+            var fc = $('.fc');
+            return function (start, end, tz, callback) {
+                //subscribe only to specified date range
+                Meteor.subscribe('events', start.toDate(), end.toDate(), function () {
+                    //trigger event rendering when collection is downloaded
+                    fc.fullCalendar('refetchEvents');
+                });
 
-            //find all, because we've already subscribed to a specific range
-            var events = Events.find().map(function (it) {
-                return {
-                    title: it.date.toISOString(),
-                    start: it.date,
-                    allDay: true
-                };
-            });
-            callback(events);
-        };
-    }
-});
+                //find all, because we've already subscribed to a specific range
+                var events = Events.find().map(function (it) {
+                    return {
+                        title: it.date.toISOString(),
+                        start: it.date,
+                        allDay: true
+                    };
+                });
+                callback(events);
+            };
+        },
+        onEventClicked: function() {
+            return function(calEvent, jsEvent, view) {
+                alert("Event clicked: "+calEvent.title);
+            }
+        }
+    });
 Template.body.rendered = function () {
     var fc = this.$('.fc');
     this.autorun(function () {
