a18d800a40429bfcc90e8f7bac233e7af9e4dde7
rzymek <rzymek@gmail.com>
2015-11-25 22:06:28 +0100

    fix #21 -  Cannot read property 'id' of null when template referenced without parameters ({{>fullcalendar}})

--- template.js
+++ template.js
@@ -1,7 +1,9 @@
 Template.fullcalendar.rendered = function() {
     var div = this.$(this.firstNode);
-    //jquery takes care of undefined values, no need to check here
-    div.attr('id',this.data.id);
-    div.addClass(this.data.class);
+    if(this.data != null) {
+        //jquery takes care of undefined values, no need to check here
+        div.attr('id', this.data.id);
+        div.addClass(this.data.class);
+    }
     div.fullCalendar(this.data);
 };
